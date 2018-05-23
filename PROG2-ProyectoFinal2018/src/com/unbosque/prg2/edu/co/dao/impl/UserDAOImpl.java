de86eededcdb9d2774eca4a5d8ce1d3269834cf2
achaparrov <39559438+achaparrov@users.noreply.github.com>
2018-05-23 18:07:12 -0500

    Update UserDAOImpl.java

--- PROG2-ProyectoFinal2018/src/com/unbosque/prg2/edu/co/dao/impl/UserDAOImpl.java
+++ PROG2-ProyectoFinal2018/src/com/unbosque/prg2/edu/co/dao/impl/UserDAOImpl.java
@@ -3,10 +3,10 @@ package com.unbosque.prg2.edu.co.dao.impl;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
 
 import com.unbosque.prg2.edu.co.dao.AuditDAO;
 import com.unbosque.prg2.edu.co.dao.UserDAO;
@@ -17,55 +17,53 @@ import com.unbosque.prg2.edu.co.util.HibernateUtil;
 
 public class UserDAOImpl implements UserDAO {
 
-	static final Logger logger = Logger.getLogger(UserDAOImpl.class);
-
 	@Override
 	public void save(User user) {
-		logger.warn("Se necesita un objeto de tipo User para poder guardarlo");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 		session.save(user);
 		t.commit();
+		
+		
+		
+		
 	}
 
 	@Override
 	public User getId(int id) {
-		logger.warn("Se necesita la id para poder buscar y dar el usuario con esa id");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		return (User) session.load(Missingsheet.class, id);
 	}
 
 	@Override
 	public User buscarUsuario(String pLogin, String pClave) {
-		logger.warn("Se necesita el login y la clave del usuario para poder buscarlo y dar el usuario con esos parametros");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Query query = session.createQuery("from User where userName = :pLogin and password= :pClave ");
-
-		query.setParameter("pLogin", pLogin);
-		query.setParameter("pClave", pClave);
-		List<?> list = query.list();		     
-		User u=null;
-		if(list.size()>0){
-			u = (User)list.get(0);		 
-		}
+	
+		        query.setParameter("pLogin", pLogin);
+		        query.setParameter("pClave", pClave);
+		        List<?> list = query.list();		     
+		        User u=null;
+		        if(list.size()>0){
+		         u = (User)list.get(0);		 
+		        }
 		return u;
 	}
-
+	
 	@Override
 	public User buscarUsuarioEmail(String pEmail) {
-		logger.warn("Se necesita el email del usuario para poder buscarlo y dar el usuario con ese email");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Query query = session.createQuery("from User where emailAddress = :pEmail ");
-
-		query.setParameter("pEmail", pEmail);		        
-		List<?> list = query.list();		     
-		User u=null;
-		if(list.size()>0){
-			u = (User)list.get(0);		 
-		}
+	
+		        query.setParameter("pEmail", pEmail);		        
+		        List<?> list = query.list();		     
+		        User u=null;
+		        if(list.size()>0){
+		         u = (User)list.get(0);		 
+		        }
 		return u;
 	}
-
+	
 	@Override
 	public List<User> list() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
@@ -77,23 +75,39 @@ public class UserDAOImpl implements UserDAO {
 
 	@Override
 	public void remove(User user) {
-		logger.warn("Se necesita un objeto de tipo User para poder removerlo");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 		session.delete(user);
 		t.commit();
-
+		
 	}
 
 	@Override
 	public void update(User user) {
-		logger.warn("Se necesita un objeto de tipo User para poder actualizarlo");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 		session.update(user);
 		t.commit();
-
+		
 	}
 
+	@Override
+	public User verificaUsuario(String usu, String pass) {
+	
+		Session session=HibernateUtil.getSessionFactory().openSession();
+		return (User)session.createQuery("FROM User u WHERE u.userName = :usu AND u.password =:pass").uniqueResult();
+	}
 
+	@Override
+	public List<User> buscarUserLogin(String userName) {
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction t = session.beginTransaction();
+		List<User> lista = session.createQuery("from User where userName = '"+userName+"'").list();
+		t.commit();
+		return lista;
+		
+	}
+
+	
 }
