57ada1f2b0bc660db12f17558dfee0babda13d8b
achaparrov <39559438+achaparrov@users.noreply.github.com>
2018-05-23 17:58:12 -0500

    Update MissingsheetDAOImpl.java

--- PROG2-ProyectoFinal2018/src/com/unbosque/prg2/edu/co/dao/impl/MissingsheetDAOImpl.java
+++ PROG2-ProyectoFinal2018/src/com/unbosque/prg2/edu/co/dao/impl/MissingsheetDAOImpl.java
@@ -2,7 +2,6 @@ package com.unbosque.prg2.edu.co.dao.impl;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -14,10 +13,8 @@ import com.unbosque.prg2.edu.co.util.HibernateUtil;
 
 public class MissingsheetDAOImpl implements MissingsheetDAO {
 
-	static final Logger logger = Logger.getLogger(MissingsheetDAOImpl.class);
 	@Override
 	public void save(Missingsheet missingsheet) {
-		logger.warn("Se necesita un objeto de tipo Missingsheet para poder guardarse");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 		session.save(missingsheet);
@@ -26,14 +23,12 @@ public class MissingsheetDAOImpl implements MissingsheetDAO {
 
 	@Override
 	public Missingsheet getUserId(int userId) {
-		logger.warn("Se necesita la id del usuario para poder dar el Missingsheet");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		return (Missingsheet) session.load(Missingsheet.class, userId);
 	}
 
 	@Override
 	public List<Missingsheet> list(int pUserId) {
-		logger.warn("Se necesita la id del usuario para poder obtener la lista de Missingsheet");
 		Session session = HibernateUtil.getSessionFactory().openSession();	
 
 		Query query = session.createQuery("from Missingsheet where userId = :pUserId");
@@ -44,7 +39,6 @@ public class MissingsheetDAOImpl implements MissingsheetDAO {
 
 	@Override
 	public void remove(Missingsheet missingsheet) {
-		logger.warn("Se necesita un objeto de tipo Missingsheet para poder removerse");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 		session.delete(missingsheet);
@@ -54,7 +48,6 @@ public class MissingsheetDAOImpl implements MissingsheetDAO {
 
 	@Override
 	public void update(Missingsheet missingsheet) {
-		logger.warn("Se necesita un objeto de tipo Missingsheet para poder actualizarse");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 		session.update(missingsheet);
