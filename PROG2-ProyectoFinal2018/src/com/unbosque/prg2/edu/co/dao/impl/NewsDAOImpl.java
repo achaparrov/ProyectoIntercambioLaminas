c694c82033ebc4aff69816ac5dc199dcf7dcfee8
achaparrov <39559438+achaparrov@users.noreply.github.com>
2018-05-23 18:00:12 -0500

    Update NewsDAOImpl.java

--- PROG2-ProyectoFinal2018/src/com/unbosque/prg2/edu/co/dao/impl/NewsDAOImpl.java
+++ PROG2-ProyectoFinal2018/src/com/unbosque/prg2/edu/co/dao/impl/NewsDAOImpl.java
@@ -2,7 +2,6 @@ package com.unbosque.prg2.edu.co.dao.impl;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -13,20 +12,17 @@ import com.unbosque.prg2.edu.co.util.HibernateUtil;
 
 public class NewsDAOImpl implements NewsDAO {
 
-	static final Logger logger = Logger.getLogger(NewsDAOImpl.class);
 	@Override
 	public void save(News news) {
-		logger.warn("Se necesita un objeto de tipo News para poder guardarse");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 		session.save(news);
 		t.commit();
-
+		
 	}
 
 	@Override
 	public News getId(int id) {
-		logger.warn("Se necesita una id para poder dar News");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		return (News) session.load(News.class, id);
 	}
@@ -42,24 +38,22 @@ public class NewsDAOImpl implements NewsDAO {
 
 	@Override
 	public void remove(News news) {
-		logger.warn("Se necesita un objeto de tipo News para poder removerlo");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 		session.delete(news);
 		t.commit();
-
+		
 	}
 
 	@Override
 	public void update(News news) {
-		logger.warn("Se necesita un objeto de tipo News para poder actualizarse");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 		session.update(news);
 		t.commit();
-
+		
 	}
-
-
+	
+	
 
 }
