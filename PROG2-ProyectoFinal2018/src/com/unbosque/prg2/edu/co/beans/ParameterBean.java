0e5c887dbf96e0451b834e589db611dc066baaaa
achaparrov <39559438+achaparrov@users.noreply.github.com>
2018-05-23 17:30:35 -0500

    Update ParameterBean.java

--- PROG2-ProyectoFinal2018/src/com/unbosque/prg2/edu/co/beans/ParameterBean.java
+++ PROG2-ProyectoFinal2018/src/com/unbosque/prg2/edu/co/beans/ParameterBean.java
@@ -1,7 +1,24 @@
 package com.unbosque.prg2.edu.co.beans;
 
-public class ParameterBean {
+import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.DataModel;
 
+import com.unbosque.prg2.edu.co.entity.Parameter;
+
+@ManagedBean
+@SessionScoped
+public class ParameterBean implements Serializable{
 	
+	@ManagedProperty("#{userBean}")
+	private UserBean userBean;
+	
+	private Parameter parameter;
+	private DataModel listaParameter;
+
+
 }
+
